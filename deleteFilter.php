797e34b1f1f6608d83bac286868d13c1a396d274
RoQuoTriX <kevin-mangold@gmx.de>
2017-05-06 21:13:43 +0200

    Filter werden per AJAX gelöscht

---
+++ deleteFilter.php
@@ -0,0 +1,10 @@
+<?php
+	$filterID = $_GET['text'];
+	include("../../zugriff.php");
+		if ($mysqli->connect_error) {
+		$message['error'] = 'Datenbankverbindung fehlgeschlagen: ' . $mysqli->connect_error;
+	}
+	$stmt="DELETE FROM Filter WHERE ID=".$filterID;
+	$mysqli->query($stmt);
+	$mysqli->close();
+?>
